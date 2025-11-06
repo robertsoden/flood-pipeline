b6f45a4da0f1918f62ee6f0ab5c4e59c7a9c58d0
robertsoden <robert.soden@gmail.com>
2025-11-05 19:24:36 -0500

    Remove redundant config and utils (now in shared/)

--- stage1-bert/config.py
+++
@@ -1,6 +0,0 @@
-# Data sources
-train_filepath = 'stage1-bert/data/bert_train_70pct.json'
-val_filepath = 'stage1-bert/data/bert_val_65.json'      
-test_filepath = 'stage1-bert/data/bert_test_30pct.json'
-extraction_filepath = 'stage1-bert/data/extraction_reserve_100.json'
-unlabeled_filepath = "data/articles_restructured.json"
